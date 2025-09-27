1ca133477ef11214f889a1e8850a51ac41514b82
ivanexist <ivanexist@gmail.com>
2025-09-27 08:27:44 +0700

    cobadeploylagi

--- src/app/components/projects/ProjectDetailsContent.tsx
+++ src/app/components/projects/ProjectDetailsContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Project } from "@/generated/prisma";
+// import { Project } from "@/generated/prisma";
 import Image from "next/image";
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
