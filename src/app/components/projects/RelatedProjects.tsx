6619113ac827f2b9d782151344040fbaa14b4e0e
ivanexist <ivanexist@gmail.com>
2025-09-28 10:26:28 +0700

    cobamingguini

--- src/app/components/projects/RelatedProjects.tsx
+++ src/app/components/projects/RelatedProjects.tsx
@@ -1,43 +1,43 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { Client, Project, Service } from "@/generated/prisma";
-import ProjectCard from "./ProjectCard"; // Adjust the import path if needed
+// import React from "react";
+// import { Client, Project, Service } from "@/generated/prisma";
+// import ProjectCard from "./ProjectCard"; // Adjust the import path if needed
 
-interface ProjectsProps {
-  projects: (Project & { Client: Client; Service: Service })[];
-}
+// interface ProjectsProps {
+//   projects: (Project & { Client: Client; Service: Service })[];
+// }
 
-// const getRelatedProjects = (
-//   { project }: ProjectDetailsProps,
-//   selectedServices: string[]
-// ) => {
-//   if (selectedServices.length === 0) return [];
-//   return projects.filter((project) =>
-//     project.services.some((service) => selectedServices.includes(service))
+// // const getRelatedProjects = (
+// //   { project }: ProjectDetailsProps,
+// //   selectedServices: string[]
+// // ) => {
+// //   if (selectedServices.length === 0) return [];
+// //   return projects.filter((project) =>
+// //     project.services.some((service) => selectedServices.includes(service))
+// //   );
+// // };
+
+// const RelatedProjects: React.FC<ProjectsProps> = ({
+//   selectedProject,
+//   projects,
+// }) => {
+//   const relatedProjectsbyServices = projects.filter(
+//     (project) =>
+//       project.Service.id === selectedProject.Service.id &&
+//       project.id !== selectedProject.id
+//   );
+
+//   return (
+//     <div>
+//       <h3>Related Projects</h3>
+//       <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
+//         {relatedProjectsbyServices.map((project) => (
+//           <ProjectCard key={project.id} project={project} />
+//         ))}
+//       </div>
+//     </div>
 //   );
 // };
 
-const RelatedProjects: React.FC<ProjectsProps> = ({
-  selectedProject,
-  projects,
-}) => {
-  const relatedProjectsbyServices = projects.filter(
-    (project) =>
-      project.Service.id === selectedProject.Service.id &&
-      project.id !== selectedProject.id
-  );
-
-  return (
-    <div>
-      <h3>Related Projects</h3>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
-        {relatedProjectsbyServices.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default RelatedProjects;
+// export default RelatedProjects;
