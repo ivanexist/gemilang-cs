fdb33c7ae59fd523aa580564a9f6d7b00152a23b
ivanexist <ivanexist@gmail.com>
2025-10-05 15:13:31 +0700

    vzcvc

--- src/app/components/home/BestServicesCard.tsx
+++ src/app/components/home/BestServicesCard.tsx
@@ -3,15 +3,12 @@
 import React from "react";
 import Link from "next/link";
 import { ReactSVG } from "react-svg";
-import { Service } from "@/store/useStore";
+import { useStore } from "@/store/useStore";
 
-type BestServiceCardProps = {
-  service: Service;
-};
-
-const BestServiceCard: React.FC<BestServiceCardProps> = ({ service }) => {
+const BestServiceCard = () => {
+  const { services } = useStore();
   return (
-    <Link href={`/services/${service.url || ""}`}>
+    <Link href={`/services/${services.url || ""}`}>
       <div className="bg-white border border-white hover:border-blue-500 hover:shadow-xl lg:m-2 py-4 px-2 relative z-10 group sm:mx-4 w-96 h-[18rem] mb-4 transition-colors duration-300">
         <div className="py-2 sm:px-4 lg:px-8 relative group my-4">
           <div className="sm:flex sm:justify-center sm:items-center my-2">
@@ -21,7 +18,7 @@ const BestServiceCard: React.FC<BestServiceCardProps> = ({ service }) => {
             />
           </div>
           <h3 className="mt-12 text-xl font-bold text-malachite-600 text-center font-PlayfairDisplay uppercase">
-            {service.name}
+            {services.name}
           </h3>
         </div>
       </div>
