fe6af2700af3aa0abfa9aa17d79cc52c73cb6b0a
ivanexist <ivanexist@gmail.com>
2025-10-09 03:14:08 +0700

    adwde

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,26 @@
 "use client";
+import { Suspense } from "react";
 import Breadcrumb from "../components/common/Breadcrumb";
 // import GridProjects from "../components/projects/GridProjects";
 import ProjectContent from "../components/projects/ProjectContent";
 
 export default function ProjectsPage() {
   return (
-    <div>
-      <Breadcrumb />
-      <div className="">
-        <ProjectContent />
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+          <div className="text-blue-600 text-xl font-semibold">
+            Loading projects...
+          </div>
+        </div>
+      }
+    >
+      <div>
+        <Breadcrumb />
+        <div className="">
+          <ProjectContent />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
